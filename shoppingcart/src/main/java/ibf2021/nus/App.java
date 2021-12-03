fb028b2526c4908e384b07304c0dcc3fa4f1b341
Y02WK <59220086+Y02WK@users.noreply.github.com>
2021-12-03 12:19:28 +0800

    Initial implementation of database directory creation and checking logic.

--- shoppingcart/src/main/java/ibf2021/nus/App.java
+++ shoppingcart/src/main/java/ibf2021/nus/App.java
@@ -1,7 +1,21 @@
 package ibf2021.nus;
 
+import java.io.File;
+import java.nio.file.Path;
+
 public class App {
     public static void main(String[] args) {
+        File dbDir;
+        if (args.length == 0) {
+            dbDir = Path.of("./db").toFile();
+        } else {
+            System.out.println(args[0]);
+            dbDir = Path.of("./%s", args[0]).toFile();
+        }
+        if (!dbDir.exists()) {
+            dbDir.mkdir();
+        }
+        
         shopCart shop = new shopCart();
         shop.openShop();
     }

880d6e231b6abd014e09bc548fd45b2da8798974
Y02WK <59220086+Y02WK@users.noreply.github.com>
2021-12-08 15:22:18 +0800

    Instantiated new ShoppingCart to run more test cases

--- shoppingcart/src/test/java/ibf2021/nus/AppTest.java
+++ shoppingcart/src/test/java/ibf2021/nus/AppTest.java
@@ -27,6 +27,7 @@ public class AppTest {
     // 2 different carts for testing
     ShoppingCart cart;
     ShoppingCart cart2;
+    ShoppingCart cart3;
     // to obtain print stream for testing
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
@@ -37,6 +38,7 @@ public class AppTest {
     public void setUp() {
         cart = new ShoppingCart("");
         cart2 = new ShoppingCart("db2");
+        cart3 = new ShoppingCart("db3");
 
         System.setOut(new PrintStream(outContent));
         System.setErr(new PrintStream(errContent));
@@ -96,4 +98,71 @@ public class AppTest {
                 "apple added to cart\npear added to cart\niphone 13 added to cart\n1. apple\n2. pear\n3. iphone 13\n",
                 outContent.toString());
     }
+
+    @Test
+    public void testLogin() {
+        cart.openShop(new ByteArrayInputStream("login kin".getBytes()));
+        assertEquals("User found. Login successful.\n", outContent.toString());
+    }
+
+    @Test
+    public void testNewLogin() {
+        cart.openShop(new ByteArrayInputStream("login tester".getBytes()));
+        cart.openShop(new ByteArrayInputStream("login tester".getBytes()));
+
+        assertEquals("User not found. Created a new login for user.\nUser found. Login successful.\n",
+                outContent.toString());
+
+    }
+
+    @Test
+    public void testBlankLogin() {
+        cart.openShop(new ByteArrayInputStream("login  ".getBytes()));
+
+        assertEquals("Username cannot be blank\n", outContent.toString());
+    }
+
+    @Test
+    public void testSaveAndLoad() {
+        // login to kin
+        cart.openShop(new ByteArrayInputStream("login kin".getBytes()));
+        // add 3 items
+        cart.openShop(new ByteArrayInputStream("add apple, pear, iphone 13".getBytes()));
+        // save cart
+        cart.openShop(new ByteArrayInputStream("save".getBytes()));
+
+        // login to kk, cart should be empty
+        cart.openShop(new ByteArrayInputStream("login kk".getBytes()));
+        assertTrue(cart.getCart().size() == 0);
+    }
+
+    @Test
+    public void testSaveAndLoad2() {
+        // login to kin
+        cart.openShop(new ByteArrayInputStream("login kin".getBytes()));
+
+        assertTrue(cart.getCart().size() == 3);
+    }
+
+    @Test
+    public void testInvalidSave() {
+        cart.openShop(new ByteArrayInputStream("save".getBytes()));
+
+        assertEquals("Error. User not logged in. Please login using 'login your_username'.\n", outContent.toString());
+    }
+
+    @Test
+    public void testEmptyUsers() {
+        cart2.openShop(new ByteArrayInputStream("users".getBytes()));
+
+        assertEquals("No users in database.\n", outContent.toString());
+    }
+
+    @Test
+    public void testUsers() {
+        cart3.openShop(new ByteArrayInputStream("users".getBytes()));
+        assertEquals("kin\nkin2\nkin3\n", outContent.toString());
+
+    }
+
 }
